a3423e663bb50ff637d86b1ebd2a2bd32d05783b
taewon0923 <ytw0740@gmail.com>
2023-05-13 14:49:16 +0900

    0513

---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,17 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+    private final MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+    private final FixDiscountPolicy discountPolicy = new FixDiscountPolicy();
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member,itemPrice);
+        return new Order(memberId,itemName,itemPrice,discountPrice);
+    }
+}
